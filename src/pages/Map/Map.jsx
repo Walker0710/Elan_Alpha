9272c456f79d6670fb5259a016187e9a8a334be7
vishanksingh <vishanksinghh@gmail.com>
2025-02-11 16:49:03 +0530

    made map

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -1,9 +1,17 @@
 import './Map.css';
 
+import map_img from '../../assets/Map/map.png';
+
+import BgCont from '../../components/BgCont/BgCont';
+
 function Map() {
   return (
     <div className="Map">
-
+      <BgCont />
+      <div className="Accommodation-Map-Heading Accommodation-Heading glow-heading">map</div>
+      <div className="Accommodation-Map-Container">
+        <img src={map_img} alt="Map" className="Accommodation-Map" />
+      </div>
     </div>
   );
 }
