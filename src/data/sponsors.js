e4ceda5b1944063585597e4468684f05128039ec
vishanksingh <vishanksinghh@gmail.com>
2025-02-20 17:10:40 +0530

    ..

---
+++ src/data/sponsors.js
@@ -0,0 +1,204 @@
+import greenko from '/src/assets/sponsors_images/Greenko logo/Greenko Logo formats/Greenko White Png.png';
+import amgreen from '/src/assets/sponsors_images/AM-GREEN.png';
+import tgpcb from '/src/assets/sponsors_images/tgpcb.jpg';
+import bv from '/src/assets/sponsors_images/bv.jpg';
+import kla from '/src/assets/sponsors_images/KLA_Logo_clr_r_rgb.png';
+
+import dkms from '/src/assets/sponsors_images/dkms b.jpg';
+import anq from '/src/assets/sponsors_images/Anq-Logo.png';
+import qoneqt from '/src/assets/sponsors_images/Qoneqt Final Logo/Qoneqt Logo Variation 01/Qoneqt_Logo_Variation_01_02.png'
+import dopastore from '/src/assets/sponsors_images/dopamine.png';
+import grabon from '/src/assets/sponsors_images/GrabOn_highres_white_primary.png';
+import ag from '/src/assets/sponsors_images/ag.jpg';
+import alumni from '/src/assets/sponsors_images/alumni.jpg';
+import baha from '/src/assets/sponsors_images/baha games logo (17).png';
+import easemytrip from '/src/assets/sponsors_images/EaseMyTrip Logo.png';
+import kapdamart from '/src/assets/sponsors_images/kapdamart.jpg';
+import klass from '/src/assets/sponsors_images/klass.jpg';
+import spykar from '/src/assets/sponsors_images/Logo Black (1).png';
+import qelica from '/src/assets/sponsors_images/qelica- New Logo.png';
+import remarkskill from '/src/assets/sponsors_images/REMARKSKILL.png';
+import sakthi from '/src/assets/sponsors_images/sakths ktchn.jpg';
+import stardust from '/src/assets/sponsors_images/stardust.jpg';
+import zebronics from '/src/assets/sponsors_images/zebr.jpg';
+
+
+
+
+
+
+
+
+
+export const title = [
+    {
+        image: greenko,
+    },
+    {
+        image: amgreen,
+    }
+];
+
+export const environment_management = [
+    {
+        image: tgpcb,
+    }
+];
+
+export const ticketing = [
+    {
+        image: bv,
+    }
+];
+
+export const techinical = [
+    {
+        image: kla,
+    }
+];
+
+export const social_welfare = [
+    {
+        image: dkms,
+    }
+];
+
+export const mobile_gaming = [
+    {
+//        image: kraf,
+    }
+];
+
+export const merchandise = [
+    {
+        image: kapdamart,
+    }
+];
+
+export const power = [
+    {
+//        image: ,
+    }
+];
+
+export const digital_banking = [
+    {
+        image: anq,
+    }
+];
+
+export const official_community = [
+    {
+        image: qoneqt,
+    }
+];
+
+export const anime_title_sponsor = [
+    {
+        image: dopastore,
+    }
+];
+
+export const banking = [
+    {
+//        image: ,
+    }
+];
+
+export const exclusive_adventure = [
+    {
+//        image: ,
+    }
+];
+
+export const style = [
+    {
+        image: spykar,
+    }
+];
+
+export const savings = [
+    {
+        image: grabon,
+    }
+];
+
+export const logistics = [
+    {
+//        image: ,
+    }
+];
+
+export const self_care = [
+    {
+//        image: ,
+    }
+];
+
+export const brand = [
+    {
+//        image: ,
+    }
+];
+
+export const oral_care = [
+    {
+        image: qelica,
+    }
+];
+
+export const travel = [
+    {
+        image: easemytrip,
+    }
+];
+
+export const school_education = [
+    {
+        image: klass,
+    }
+];
+
+export const audio = [
+    {
+        image: zebronics,
+    }
+];
+
+export const food = [
+    {
+        image: sakthi,
+    }
+];
+
+export const event = [
+    {
+        image: stardust,
+    },
+    {
+//        image: ,
+    },
+];
+
+export const fest_workshop = [
+    {
+        image: remarkskill,
+    }
+];
+
+export const wooden_games = [
+    {
+        image: baha,
+    }
+];
+
+export const special_thanks = [
+    {
+        image: ag,
+    },
+    {
+        image: alumni,
+    },
+];
+
+
--- src/pages/Sponsors/Sponsors.jsx
+++ src/pages/Sponsors/Sponsors.jsx
@@ -1,8 +1,392 @@
 import './Sponsors.css';
 
+import {
+  title,
+  environment_management,
+  ticketing,
+  techinical,
+  social_welfare,
+  mobile_gaming,
+  merchandise,
+  power,
+  digital_banking,
+  official_community,
+  anime_title_sponsor,
+  banking,
+  exclusive_adventure,
+  style,
+  savings,
+  logistics,
+  self_care,
+  brand,
+  oral_care,
+  travel,
+  school_education,
+  audio,
+  food,
+  event,
+  fest_workshop,
+  wooden_games,
+  special_thanks
+
+} from '../../data/sponsors.js';
+
+import BgCont from '../../components/BgCont/BgCont';
+
 function Sponsors() {
   return (
     <div className="Sponsors">
+      <BgCont />
+      <div className="Sponsors-Heading glow-heading">our sponsors</div>
+
+      <div className="Sponsors-Member-Headings">title sponsors</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          title.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Technical Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          techinical.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+      <div className="Sponsors-Member-Headings">Social Welfare Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          social_welfare.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+      <div className="Sponsors-Member-Headings">Ticketing Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          ticketing.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Environment Management Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          environment_management.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Power Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          power.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+      
+      <div className="Sponsors-Member-Headings">Digital Banking Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          digital_banking.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Official Community Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          official_community.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Title Sponsor of Anime Quiz</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          anime_title_sponsor.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Savings Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          savings.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Travel Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          travel.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Mobile Gaming Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          mobile_gaming.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Style Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          style.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Exclusive Adventure Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          exclusive_adventure.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+      
+      <div className="Sponsors-Member-Headings">Logistics Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          logistics.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Self-care Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          self_care.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+      
+      <div className="Sponsors-Member-Headings">Fest Workshop Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          fest_workshop.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Banking Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          banking.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Merchandise Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          merchandise.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Brand Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          brand.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Event Partner </div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          event.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">School Education Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          school_education.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Food Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          food.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Audio Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          audio.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Wooden Games Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          wooden_games.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Oral Care Partner</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          oral_care.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
+
+      <div className="Sponsors-Member-Headings">Special thanks to</div>
+      <div className="Sponsors-Member-Boxes-Container">
+        {
+          special_thanks.map((person, index) => (
+            <div className="Sponsors-Member-Box" key={index}>
+              <img src={person.image} className='Sponsors-Member-Img'></img>
+              
+            </div>
+          ))
+        }
+      </div>
+
 
     </div>
   );
